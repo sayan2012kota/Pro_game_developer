202910c14f69b886ea7c46ad24c384f682d94d07
sayan2012kota <sayankota2012@gmail.com>
2025-08-03 09:12:52 +0100

    Add files via upload

--- circle.py
+++ circle.py
@@ -3,14 +3,29 @@ pygame.init()
 screen = pygame.display.set_mode((900, 300))
 pygame.display.set_caption("Circle")
 
+
+class Circle():
+    def __init__(self, colour, dim):
+        self.circle_surface = screen
+        self.circle_colour = colour
+        self.circle_dimension = dim
+
+    def draw_circle(self):
+        pygame.draw.circle(self.circle_surface, self.circle_colour, self.circle_dimension, 150, 20)
+
+screen.fill("white")
+
+circle = Circle("red", (200, 150))
+circle.draw_circle()
+
+pygame.display.update()
+
 run = True
 
 while run == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-#class Circle()
 
 
 
