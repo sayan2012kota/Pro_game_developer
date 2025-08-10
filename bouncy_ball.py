cda2288136dbf1826dab402f4a750164e0dc32db
sayan2012kota <sayankota2012@gmail.com>
2025-08-10 09:25:13 +0100

    Add files via upload

--- bouncy_ball.py
+++ bouncy_ball.py
@@ -4,37 +4,42 @@ TITLE = "Bouncy Ball"
 WIDTH = 500
 HEIGHT = 800
 
-colour = "blue"
-
 GRAVITY = 2000
 
 class Ball():
-    def __init__(self, x, y):
+    def __init__(self, x, y, radius, colour):
         self.x = x
         self.y = y
         self.vx = 200
         self.vy = 0
-        self.radius = 25
-
+        self.radius = radius
+        self.colour = colour
 
 
     def draw_ball(self):
-        screen.draw.filled_circle((self.x, self.y), self.radius, colour)
+        screen.draw.filled_circle((self.x, self.y), self.radius, self.colour)
 
-ball = Ball(100, 50)
 
+ball = Ball(100, 50, 25, "blue")
+ball_2 = Ball(300, 50, 50, "red")
 
 
 def draw():
     screen.clear()
     ball.draw_ball()
+    #ball_2.draw_ball()
 
 def update(dt):
     uy = ball.vy
     ball.vy = ball.vy + GRAVITY * dt
     ball.y = ball.y + (uy + ball.vy) * 0.5 * dt
-    
 
+    ball.x = ball.x + ball.vx * dt
+    if ball.x >= WIDTH or ball.x <= 0:
+        ball.vx = -ball.vx
+    if ball.y >= HEIGHT:
+        ball.y = HEIGHT - ball.radius
+        ball.vy = -ball.vy * 0.9
 
 
 pgzrun.go()
