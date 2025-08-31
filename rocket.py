f584ce784a613497686f84bb09f98a5933161f64
sayan2012kota <sayankota2012@gmail.com>
2025-08-31 10:02:17 +0100

    Add files via upload

--- rocket.py
+++ rocket.py
@@ -1,13 +1,46 @@
 from pygame.locals import *
 import pygame
+from time import *
 pygame.init()
 
 screen = pygame.display.set_mode((700, 500))
 
+rocket = pygame.image.load("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/rocket_image.png")
+space_bg = pygame.image.load("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/space_background.png")
 
+keys = [False, False, False, False]
+
+x = 350
+y = 250
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+        if event.type == KEYDOWN:
+            if event.type == K_UP:
+                keys[0] = True
+            elif event.type == K_RIGHT:
+                keys[1] = True
+            elif event.type == K_LEFT:
+                keys[2] = True
+            elif  event.type == K_DOWN:
+                keys[3] = True
+        if event.type == KEYUP:
+            if event.type == K_UP:
+                keys[0] = False
+            elif event.type == K_RIGHT:
+                keys[1] = False
+            elif event.type == K_LEFT:
+                keys[2] = False
+            elif  event.type == K_DOWN:
+                keys[3] = False
+    if keys[0]:
+        y = y + 1
+    screen.blit(space_bg, (0,0))
+    screen.blit(rocket, (x, y))
+    pygame.display.update()
+    y = y + 1 
+    sleep(0.05)
+
     
