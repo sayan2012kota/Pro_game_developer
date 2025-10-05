00f5613f968e47bce67db8a8e6c25ffeccde7e82
sayan2012kota <sayankota2012@gmail.com>
2025-10-05 09:54:15 +0100

    Add files via upload

--- spaceship.py
+++ spaceship.py
@@ -1,9 +1,13 @@
 import pygame
 from pygame.locals import *
 pygame.font.init()
+pygame.mixer.init()
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 800))
+
+bullet_hit_sound = pygame.mixer.Sound("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/Gun+Silencer.mp3")
+bullet_fire_sound = pygame.mixer.Sound("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/Grenade+1.mp3")
 
 yellow_spaceship = pygame.image.load("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/spaceship_yellow.png")
 red_spaceship = pygame.image.load("C:/Users/pooja/Desktop/pro_game_development/Game_developer2/lesson_2/spaceship_red.png")
@@ -16,6 +20,8 @@ red_spaceship = pygame.transform.rotate(red_spaceship_resize, (270))
 H_font = pygame.font.SysFont("Calibra", 40)
 
 rect = pygame.Rect(495, 0, 10, 800)
+yellow_bullet_hit = pygame.USEREVENT+1
+red_bullet_hit = pygame.USEREVENT+2
 
 def draw_window(Y_rect, R_rect, R_bullets, Y_bullets):
     screen.blit(space_bg, (0,0))
@@ -52,6 +58,24 @@ def move_r(red_keys, R_rect):
     if red_keys[pygame.K_s] and R_rect.y < 800:
         R_rect.y = R_rect.y + 10
 
+def move_bullets(Y_bullets, R_bullets, Y_rect, R_rect):
+    for bullet in Y_bullets:
+        bullet.x = bullet.x + velocity
+        if bullet.colliderect(R_rect):
+            pygame.event.post(pygame.event.Event(yellow_bullet_hit))
+            Y_bullets.remove(bullet)
+        elif bullet.x > 1000:
+            Y_bullets.remove(bullet)
+    for bullet in R_bullets:
+        bullet.x = bullet.x - velocity
+        if bullet.colliderect(Y_rect):
+            pygame.event.post(pygame.event.Event(red_bullet_hit))
+            R_bullets.remove(bullet)
+        elif bullet.x < 0:
+            R_bullets.remove(bullet)
+
+Y_health = 10
+R_health = 10
 clock = pygame.time.Clock()
 Y_rect = pygame.Rect(200, 400, 50, 40)
 R_rect = pygame.Rect(800, 400, 50, 40)
@@ -61,6 +85,8 @@ velocity = 8
 
 yellow_health = 10
 red_health = 10
+
+winner = ""
 
 Y_bullets = []
 R_bullets = []
@@ -74,12 +100,32 @@ while True:
             if event.key == K_LCTRL and len(R_bullets) < max_bullets:
                 bullet = pygame.Rect(R_rect.x, R_rect.y + 20, 10, 5)
                 R_bullets.append(bullet)
+                bullet_fire_sound.play()
             if event.key == K_RCTRL and len(Y_bullets) < max_bullets:
                 bullet2 = pygame.Rect(Y_rect.x + 40, Y_rect.y + 20, 10, 5)
                 Y_bullets.append(bullet2)
+                bullet_fire_sound.play()
+        if event.type == red_bullet_hit:
+            Y_health = Y_health - 1
+            bullet_hit_sound.play()
+        if event.type == yellow_bullet_hit:
+            R_health = R_health - 1
+            bullet_hit_sound.play()
+    if R_health <= 0:
+        winner = "yellow spaceship wins"
+        text_colour = "yellow"
+    if Y_health <= 0:
+        winner = "red spaceship wins"
+        text_colour = "red"
+    if winner != "":
+        wintext = H_font.render(winner, 1, text_colour) 
+        screen.blit(wintext, (500, 400)) 
+        pygame.display.update()
+        pygame.time.delay(6000) 
+        break    
     key_press = pygame.key.get_pressed()
-    Y_health = 10
-    R_health = 10
+    move_bullets(Y_bullets, R_bullets, Y_rect, R_rect)
+
     move_y(key_press, Y_rect)
     move_r(key_press, R_rect)
     draw_window(Y_rect, R_rect, R_bullets, Y_bullets)
